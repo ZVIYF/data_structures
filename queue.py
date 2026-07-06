eac329d2348cf9c1331f2c293f9cd273df3531f3
צבי פישר <zviyf132@gmail.com>
2026-07-06 16:11:12 +0300

    queue exercise 4

--- queue.py
+++ queue.py
@@ -1,8 +1,38 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append(self, val):
+        self.tail.next = Node(val)
+        self.tail = self.tail.next
+        if self.head is None:
+            self.head = self.tail
+
+    def removefirst(self):
+        if self.head is None:
+            return
+        temp = self.head
+        self.head = temp.next
+        if self.head is None:
+            self.tail = None
+        return temp.data
+
+    def peek(self):
+        if self.head:
+            return self.head.data
+
 # # # # # # #
 
 class Queue:
     def __init__(self):
-        self.queue = Link
+        self.queue = LinkedList()
         self.len = -1
 
     def enqueue(self, val):
@@ -11,7 +41,7 @@ class Queue:
 
     def dequeue(self):
         self.len -= 1
-        return self.queue.pop(0)
+        return self.queue.removefirst()
 
     def is_empty(self):
         if self.len < 0:
@@ -19,9 +49,7 @@ class Queue:
         return False
 
     def peek(self):
-        if self.is_empty():
-            return
-        return self.queue[0]
+        return self.queue.peek()
 
 # # # # # # # # # #
 
@@ -31,8 +59,17 @@ class ClassifiedRequests(Queue):
             return True
         return False
 
+    def is_late(self):
+        """
+        אם שעת סגירה - פחות דלתא של שעה < קטן מ זמן נוכחי:
+
+        :return:
+        """
+        pass
+
+
     def enqueue(self, val):
-        if self.is_full():
+        if self.is_full() and self.is_late():
             print("Queue if full. No more requests today!")
             return
         self.queue.append(val)
@@ -40,7 +77,37 @@ class ClassifiedRequests(Queue):
 
 # # # # # # # # # #
 
-def IntQueue(q:Queue):
+def GapIntQueue(q:Queue):
+    temp = Queue()
     q1 = Queue()
+    temp_val = None
+    while not q.is_empty():
+        if temp_val:
+            q1.enqueue(temp_val - q.peek())
+        temp_val = q.dequeue()
+        temp.enqueue(temp_val)
+    q = temp
+    return q1
 
-    while not q2.is_empty():
+def replace_values(q:Queue, value, replace):
+    temp_q = Queue()
+    while not q.is_empty():
+        temp_val = q.dequeue()
+        if temp_val == value:
+            temp_val = replace
+        temp_q.enqueue(temp_val)
+    q = temp_q
+    return q
+
+def sort_queue(q:Queue):
+    temp_q1 = Queue()
+    temp_q2 = Queue()
+    while not q.is_empty():
+        if q.peek() < 0:
+            temp_q2.enqueue(q.dequeue())
+        else:
+            temp_q1.enqueue(q.dequeue())
+    while not temp_q2.is_empty():
+        temp_q1.enqueue(temp_q2.dequeue())
+    return temp_q1
+
