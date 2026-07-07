46111ac24b015951077c4dd665e654e53976a7a0
צבי פישר <zviyf132@gmail.com>
2026-07-07 09:22:07 +0300

    binary tree

--- binary_tree.py
+++ binary_tree.py
@@ -12,7 +12,26 @@ class BinaryTree:
     def append(self, val):
         self.root = BinaryTree.insert(self.root, val)
 
+    @staticmethod
+    def list_to_binary_tree(my_list):
+        my_tree = BinaryTree()
+        for val in list:
+            my_tree.append(val)
+        return my_tree
+
     def search(self, val):
+        return self.search_rec(self.root, val)
+
+    def print_in_order(self):
+        self.print_rec(self.root)
+
+    @staticmethod
+    def print_rec(node):
+        if node is None:
+            return
+        BinaryTree.print_rec(node.left)
+        print(node.data)
+        BinaryTree.print_rec(node.right)
 
     @staticmethod
     def insert(node, val):
@@ -24,3 +43,18 @@ class BinaryTree:
         else:
             node.right = BinaryTree.insert(node.right, val)
             return node
+
+    @staticmethod
+    def search_rec(node, val):
+        if node is None:
+            return False
+        if node.data == val:
+            return True
+        if node.data > val:
+            return BinaryTree.search_rec(node.left, val)
+        return BinaryTree.search_rec(node.right, val)
+
+
+arr1 = [9, 1, 7, 3, 5]
+tree1 = BinaryTree.list_to_binary_tree(arr1)
+BinaryTree.print_in_order(tree1)
