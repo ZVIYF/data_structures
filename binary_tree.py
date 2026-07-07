4c0acf9378f288acd96b429b231f15fd2830a4e1
צבי פישר <zviyf132@gmail.com>
2026-07-07 10:22:21 +0300

    exrcise 9

--- binary_tree.py
+++ binary_tree.py
@@ -1,4 +1,4 @@
-class Node:
+class TreeNode:
     def __init__(self, val):
         self.data = val
         self.left = None
@@ -15,28 +15,41 @@ class BinaryTree:
     @staticmethod
     def list_to_binary_tree(my_list):
         my_tree = BinaryTree()
-        for val in list:
+        for val in my_list:
             my_tree.append(val)
         return my_tree
 
     def search(self, val):
         return self.search_rec(self.root, val)
 
+    def print_pre_order(self):
+        self.print_pre_order_rec(self.root)
+        print()
+
     def print_in_order(self):
-        self.print_rec(self.root)
+        self.print_in_order_rec(self.root)
+        print()
 
     @staticmethod
-    def print_rec(node):
+    def print_pre_order_rec(node):
         if node is None:
             return
-        BinaryTree.print_rec(node.left)
-        print(node.data)
-        BinaryTree.print_rec(node.right)
+        print(node.data, end=", ")
+        BinaryTree.print_pre_order_rec(node.left)
+        BinaryTree.print_pre_order_rec(node.right)
+
+    @staticmethod
+    def print_in_order_rec(node):
+        if node is None:
+            return
+        BinaryTree.print_in_order_rec(node.left)
+        print(node.data, end=", ")
+        BinaryTree.print_in_order_rec(node.right)
 
     @staticmethod
     def insert(node, val):
         if node is None:
-            return Node(val)
+            return TreeNode(val)
         if node.data > val:
             node.left = BinaryTree.insert(node.left, val)
             return node
@@ -54,7 +67,44 @@ class BinaryTree:
             return BinaryTree.search_rec(node.left, val)
         return BinaryTree.search_rec(node.right, val)
 
+    def nodes_count(self):
+        return self.nodes_count_rec(self.root)
+
+    @staticmethod
+    def nodes_count_rec(node):
+        if node is None:
+            return 0
+        return 1 + BinaryTree.nodes_count_rec(node.left) + BinaryTree.nodes_count_rec(node.right)
+
+    def leafs_count(self):
+        return self.leafs_count_rec(self.root)
+
+    @staticmethod
+    def leafs_count_rec(node):
+        if node is None:
+            return 0
+        if node.left is None and node.right is None:
+            return 1
+        else:
+            return 0 + BinaryTree.leafs_count_rec(node.left) + BinaryTree.leafs_count_rec(node.right)
+
+    def levels_count(self):
+        return self.levels_count_rec(self.root)
+
+    @staticmethod
+    def levels_count_rec(node):
+        if node is None:
+            return 0
+        left = 1 + BinaryTree.levels_count_rec(node.left)
+        right = 1 + BinaryTree.levels_count_rec(node.right)
+        level = max(left, right)
+        return level
+
+
+
 
 arr1 = [9, 1, 7, 3, 5]
 tree1 = BinaryTree.list_to_binary_tree(arr1)
-BinaryTree.print_in_order(tree1)
+tree1.print_in_order()
+tree1.print_pre_order()
+print(tree1.nodes_count())
